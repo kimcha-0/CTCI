297562cfc25cfc2ca197ec01789e011058a94998
David <davidcha@email.unc.edu>
2024-01-03 12:20:59 -0500

    using .join() with array to avoid quadratic time complexity for string concatenation

--- arrays-strings/stringCompression.py
+++ arrays-strings/stringCompression.py
@@ -3,14 +3,19 @@
 
 def str_compress(s):
     # space: O(n)
-    # time: O(n)
+    # time: O(n^2) when accounting for string concatenation in python
+    # time: O(n) when using array and joining
     map = {}
-    result = ""
+    result = []
     for i in s:
         if i in map:
             map[i] += 1
         else:
             map[i] = 0
     for key in map:
-        result += i+str(map[key])
-    return result
+        result.append(key)
+        result.append(str(map[key]))
+    return ''.join(result)
+
+
+print(str_compress('aabcccccaaa'))

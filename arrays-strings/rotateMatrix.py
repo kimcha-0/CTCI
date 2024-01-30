48bc742f792473d2fd8ba19a4382853d57f08ed3
David <davidcha@email.unc.edu>
2024-01-29 23:52:51 -0500

    brute force solution of rotateMatrix done

--- arrays-strings/rotateMatrix.py
+++ arrays-strings/rotateMatrix.py
@@ -1,2 +1,14 @@
 """Rotate an N x N matrix by 90 degrees can we do this in place?"""
 
+
+def rotateMatrix(matrix):
+    # space: O(n^2)
+    # time: O(n^2)
+    result = [[0]*len(matrix[0]) for i in range(len(matrix))]
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            result[i][j] = matrix[-j][i]
+    #  (x, y) -> (y, -x)
+    return result
+
+print(rotateMatrix([[1, 1, 1], [0, 0, 0], [0, 0, 0]]))
